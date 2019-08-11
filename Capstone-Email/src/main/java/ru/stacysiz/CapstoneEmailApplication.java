709515d8fa7609740ded25e84f7093121139a97f
Stacy Sizova <nastyasizova@gmail.com>
2019-08-11 22:36:35 +0300

    ail microservice added
    add email module

---
+++ Capstone-Email/src/main/java/ru/stacysiz/CapstoneEmailApplication.java
@@ -0,0 +1,13 @@
+package ru.stacysiz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapstoneEmailApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CapstoneEmailApplication.class, args);
+	}
+
+}
