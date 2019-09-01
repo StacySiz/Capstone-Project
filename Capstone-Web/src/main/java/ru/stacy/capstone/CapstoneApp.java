6e0b4ee5e4b2f4116d4fd111273cf013c39ada49
Stacy Sizova <nastyasizova@gmail.com>
2019-09-01 21:26:34 +0300

    add eureka clients configs

--- Capstone-Web/src/main/java/ru/stacy/capstone/CapstoneApp.java
+++ Capstone-Web/src/main/java/ru/stacy/capstone/CapstoneApp.java
@@ -1,19 +1,14 @@
 package ru.stacy.capstone;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import ru.stacy.capstone.service.UserService;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class CapstoneApp {
-
-  @Autowired
-  private UserService userService;
 
   public static void main(String[] args) {
     SpringApplication.run(CapstoneApp.class, args);
   }
-
-
 }
